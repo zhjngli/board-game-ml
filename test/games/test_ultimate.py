ead925b2d2083d51d10f803cabacc15b8bcba69c
Zhijiang Li <zl256@cornell.edu>
2024-03-13 15:14:26 -0700

    add another unit test

--- test/games/test_ultimate.py
+++ test/games/test_ultimate.py
@@ -7,7 +7,7 @@ from games.ultimate_ttt.ultimate import UltimateState, UltimateTicTacToe
 def test_unfinished():
     b = np.asarray(
         [
-            [
+            [  # technically p1 has 2 mini wins, but board not filled
                 [[1, 0, 0], [1, 0, 0], [1, 0, 0]],  # <- this line is nonant 1
                 [[0, -1, 0], [0, -1, 0], [0, -1, 0]],
                 [[0, 0, 1], [0, 0, 1], [0, 0, 1]],
@@ -83,6 +83,35 @@ def test_p1_3_in_a_row():
     assert UltimateTicTacToe._is_win(P1, b)
     assert not UltimateTicTacToe._is_win(P2, b)
     assert UltimateTicTacToe._is_board_filled(b)
+    assert UltimateTicTacToe.check_finished(state)
+
+
+def test_p2_3_in_a_row():
+    b = np.asarray(
+        [
+            [
+                [[1, 0, 0], [1, 0, 0], [1, 0, 0]],
+                [[0, -1, 0], [0, -1, 0], [0, -1, 0]],
+                [[0, 0, 1], [0, 0, 1], [0, 0, 1]],
+            ],
+            [
+                [[-1, 0, 0], [-1, 0, 0], [-1, 0, 0]],
+                [[-1, 1, -1], [1, 1, -1], [1, -1, -1]],
+                [[0, 0, -1], [0, 0, -1], [0, 0, -1]],
+            ],
+            [
+                [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+                [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+                [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+            ],
+        ]
+    )
+    state = UltimateState(board=b, player=P1, active_nonant=None)
+    assert not UltimateTicTacToe._3_in_a_row(P1, b)
+    assert not UltimateTicTacToe._is_win(P1, b)
+    assert UltimateTicTacToe._3_in_a_row(P2, b)
+    assert UltimateTicTacToe._is_win(P2, b)
+    assert not UltimateTicTacToe._is_board_filled(b)
     assert UltimateTicTacToe.check_finished(state)
 
 
